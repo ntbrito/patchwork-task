50faf803ef150e4b73812b90061425affe17e740
Nelson Brito <nelsonb@datainterconnect.co.uk>
2023-10-24 18:07:38 +0100

    docker context

--- devops-test-main/deploy.sh
+++ devops-test-main/deploy.sh
@@ -9,6 +9,6 @@ region=$2
 
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
 
-docker build . -t webapp
+docker build . -t webapp -f devops-test-main/Dockerfile
 docker tag webapp:latest ${account_id}.dkr.ecr.${region}.amazonaws.com/patchwork/webapp:latest
 docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/patchwork/webapp:latest
